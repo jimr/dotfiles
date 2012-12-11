25317cd10635d2c3db4f28e5e9365fee55119ee6
James Rutherford <jim@jimr.org>
2012-12-11 09:22:27 +0000

    added brightness up/down mappings

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -37,6 +37,8 @@ main = do
         , ((mod4Mask, xK_p), spawn "dmenu_run")
         , ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
         , ((mod4Mask, xK_x), spawn "xmodmap /home/jrutherford/.Xmodmap")
+        , ((mod1Mask, xK_Down), spawn "/home/jrutherford/bin/brightness.sh -10")
+        , ((mod1Mask, xK_Up), spawn "/home/jrutherford/bin/brightness.sh +10")
         , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
         , ((0, xK_Print), spawn "scrot")
         ]
